06073a6927c5b84ccbc5ec779d2f8e212c480f79
ElijahDavila <elijahdavila96@gmail.com>
2020-06-22 11:45:38 -0400

    SpringSiteLab

--- demo/src/main/java/com/example/demo/MainController.java
+++ demo/src/main/java/com/example/demo/MainController.java
@@ -1,13 +1,16 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
     
     @GetMapping("/register")
-    public String showForm() {
+    public String showForm(Model model) {
+        user user = new user();
+        model.addAttribute("user", user);
         return "register_form";
     }
 }
