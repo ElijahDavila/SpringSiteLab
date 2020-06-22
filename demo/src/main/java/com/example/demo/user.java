64e3086166a55fb646f8ac2f7b0dc91ce78ac98a
ElijahDavila <elijahdavila96@gmail.com>
2020-06-21 21:45:50 -0400

    SpringSiteLab

---
+++ demo/src/main/java/com/example/demo/user.java
@@ -0,0 +1,49 @@
+package com.example.demo;
+
+public class user {
+    private String name;
+    private String email;
+    private String username;
+    private String password;
+    private String DoB;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDoB() {
+        return DoB;
+    }
+
+    public void setDoB(String doB) {
+        DoB = doB;
+    }
+}
